0265a0727a18447e33e7c9e85336eedf0e509f22
RayCodes50 <przemox2@gmail.com>
2026-04-02 13:01:18 +0100

    Create start of menu with photos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
-import { greeting, loadHome } from "./greeting.js";
+import { greeting, loadHome } from "./home.js";
+import { loadMenu } from "./menu.js";
 
 console.log(greeting);
-loadHome();
+// loadHome();
